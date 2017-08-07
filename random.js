5a2b4babde7abf9e9cfaca207d67e8cfd866cbdb
Alex Uvere <alexuvere@gmail.com>
2017-08-07 15:33:07 -0500

    updated js

--- random.js
+++ random.js
@@ -1,4 +1,12 @@
-function getRandomNumber() {
-  var randomNumber = Math.floor( Math.random() * 6 ) + 1;
-  return randomNumber;
+functuon getRandomNumber (lower, upper)
+ {
+
+math.floor(Math.random() * (upper -lower + 1)) + lower;
+return random;
+
 }
+
+console.log (getRandomNumber (1,6));
+console.log (getRandomNumber (1, 100));
+console.log (getRandomNumber (100, 20000));
+console.log (getRandomNumber (50, 100,));
